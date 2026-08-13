5f1d5a792976b27953b7a36e53e83d3fd4a9b593
Parianshi <parianshi687@gmail.com>
2026-08-13 17:43:00 +0530

    Time: 3 ms (86.08%) | Memory: 43.4 MB (25.92%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk= new Stack<>();
+
+        for(char ch: s.toCharArray()){
+
+            if(ch=='(' || ch=='{' || ch=='['){
+                stk.push(ch);
+            }
+            else{
+                if(stk.isEmpty()) return false;
+                char top= stk.peek();
+                if( ch==')'&& top=='(' || ch=='}'&& top=='{' || ch==']'&& top=='[' ){
+                    stk.pop();
+                } else{
+                    return false;
+                }
+            }  
+        }
+
+        if(stk.isEmpty()) return true;
+        else return false;
+    }
+}
