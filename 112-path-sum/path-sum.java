d3eb1a365b7f92b43b76ceebef05b0b30023c402
Parianshi <parianshi687@gmail.com>
2026-08-19 19:04:27 +0530

    Time: 2 ms (3.14%) | Memory: 45.3 MB (5.01%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+
+        if(root==null) return false;
+        
+        Stack<TreeNode> path= new Stack<>();
+        Stack<Integer> sumPath= new Stack<>();
+
+        path.push(root);
+        sumPath.push(root.val);
+
+        while(!path.isEmpty()){
+            TreeNode temp=path.pop();
+            int tempVal= sumPath.pop();
+
+            if(temp.left==null && temp.right==null && tempVal==sum) return true;
+
+            if(temp.left!=null){
+                path.push(temp.left);
+                sumPath.push(temp.left.val+tempVal);
+            }
+
+            if(temp.right!=null){
+                path.push(temp.right);
+                sumPath.push(temp.right.val+tempVal);
+            }
+        }
+        return false;
+    }
+}
