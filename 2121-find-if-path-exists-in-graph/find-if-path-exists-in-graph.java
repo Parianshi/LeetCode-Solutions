b3f438cdd0849204205e0c0b636f57d05900d118
Parianshi <parianshi687@gmail.com>
2026-09-02 01:30:58 +0530

    Time: 55 ms (87.60%) | Memory: 281.7 MB (55.55%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,33 +1,34 @@
 class Solution {
-
-    public boolean dfs(List<List<Integer>> adjList, int source, int destination, boolean [] vis){
-        if(source==destination) return true;
-        vis[source]=true;
-
-        for(int neighbour : adjList.get(source)){
-            if(!vis[neighbour]){
-                if(dfs(adjList, neighbour, destination, vis)) return true;
-            }
-        }
-        return false;
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> adjList= new ArrayList<>();
 
+        List<List<Integer>> adjList= new ArrayList<>();
         for(int i=0;i<n;i++){
             adjList.add(new ArrayList<>());
         }
 
         for(int []edge: edges){
             int u=edge[0], v=edge[1];
-
             adjList.get(u).add(v);
             adjList.get(v).add(u);
         }
 
         boolean []vis= new boolean[n];
 
-        return dfs(adjList, source, destination, vis);
+        Queue<Integer> q= new LinkedList<>();
+        q.add(source);
+
+        while(!q.isEmpty()){
+            int val= q.poll();
+            if(val==destination) return true;
+            for(int neighbour: adjList.get(val)){
+                if(!vis[neighbour]){
+                    q.add(neighbour);
+                    vis[neighbour]=true;
+                }
+            }
+        }
+
+        return false;
         
     }
 }
