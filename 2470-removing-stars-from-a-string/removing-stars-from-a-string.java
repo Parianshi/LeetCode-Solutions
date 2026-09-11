9616d34bb45b98acfcc044f807efdcc3ef1b8750
Parianshi <parianshi687@gmail.com>
2026-09-11 23:19:56 +0530

    Time: 64 ms (55.09%) | Memory: 48 MB (72.61%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeStars(String s) {
+
+        Stack<Character> stk= new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch=='*'){
+                stk.pop();
+            } else {
+                stk.push(ch);
+            }
+        }
+
+        StringBuilder sb= new StringBuilder();
+        for(char ch: stk){
+            sb.append(ch);
+        }
+
+        return sb.toString();
+
+    }
+}
