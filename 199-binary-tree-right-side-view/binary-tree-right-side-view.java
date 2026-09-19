b9bdc9ff5e2eaaaf30de82de383d198136457d01
Parianshi <parianshi687@gmail.com>
2026-09-19 11:18:56 +0530

    Time: 1 ms (71.72%) | Memory: 43.2 MB (97.90%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -22,17 +22,13 @@ class Solution {
 
         while(!q.isEmpty()){
             int size=q.size();
-            int lastVal=0;
             for(int i=0;i<size;i++){
-                TreeNode node= q.poll();
-                lastVal=node.val;
+                TreeNode node=q.poll();
+                if(i==size-1) ans.add(node.val);
                 if(node.left!=null) q.add(node.left);
                 if(node.right!=null) q.add(node.right);
             }
-            ans.add(lastVal);
         }
         return ans;
-
-        
     }
 }
