6573b32ea5d71e9312b89e4d2909eca73a77c279
Parianshi <parianshi687@gmail.com>
2026-09-19 11:32:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (79.25%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -22,16 +22,17 @@ class Solution {
         Boolean rev=false;
 
         while(!q.isEmpty()){
-            int size= q.size();
-            List<Integer> arr= new ArrayList<>();
+            int size=q.size();
+            List<Integer> n=new ArrayList<>();
             for(int i=0;i<size;i++){
-                TreeNode node= q.poll();
-                arr.add(node.val);
+                TreeNode node=q.poll();
+                n.add(node.val);
                 if(node.left!=null) q.add(node.left);
                 if(node.right!=null) q.add(node.right);
             }
-            if(rev) Collections.reverse(arr);
-            ans.add(arr);
+            
+            if(rev) Collections.reverse(n);
+            ans.add(n);
             rev=!rev;
         }
         return ans;
